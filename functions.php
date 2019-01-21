9dba8249d43e4271e9ad89b15d5d14ce6724fb48
Kevin Bollman <kevinbollman@gmail.com>
2019-01-20 18:56:29 -0600

    added end point to handle calendar get latest 3 events

--- functions.php
+++ functions.php
@@ -8,3 +8,32 @@ function my_footer_scripts(){
     	wp_enqueue_style( 'customcss', get_stylesheet_directory_uri() . '/staticresources/custom.css' ); 
 	wp_enqueue_script( 'customjs', get_stylesheet_directory_uri() . '/staticresources/custom.js' );
 }
+
+/**
+ * Grab latest event posts
+ *
+ * @param array $data Options for the function.
+ * @return $object, or null if none.  */
+ 
+	 function get_latest_events ( $params ){
+  	 $post = get_posts( array(
+		'post_type' => $params['post_type'],
+            	'posts_per_page'  => 3,
+            	'offset'      => 0
+      ) );
+
+ 
+  	 	if( empty( $post ) ){
+  	 		return null;
+  	 	}
+ 		return $post;
+  	 	//return $post[0]->post_title;
+  	 }
+
+	 // Register the rest route here.
+  	 add_action( 'rest_api_init', function () {     
+            register_rest_route( 'calendar/v1', 'latest-events',array( 
+                'methods'  => 'GET',
+                'callback' => 'get_latest_events'
+            ) );
+     } );  
