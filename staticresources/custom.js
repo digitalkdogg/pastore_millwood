79573deae3ceae33877f026610ac70996d9f878c
digitalkdogg <digitalkdogg@gmail.com>
2021-01-31 11:34:03 -0600

    improved header on mobile

--- staticresources/custom.js
+++ staticresources/custom.js
@@ -1068,6 +1068,7 @@ var millwood;
 	if ($('.top_panel_title_inner').length > 0) {
 		let bgimg = $('.top_panel_title_inner').css('background-image');
 		$('.top_panel_wrap_inner').css({'background-image': bgimg})
+		$('.header_mobile').css({'background-image': bgimg})
 
 		$('.top_panel_title_inner').css({'background-image': 'none'})
 	}
